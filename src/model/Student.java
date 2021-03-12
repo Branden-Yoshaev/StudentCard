de7e2e753d21b27e65279d3ff4fdf4efb44aefff
Branden-Yoshaev <bsyoshae@lakeheadu.ca>
2021-03-12 10:46:09 -0500

    Commit to the Student Class

---
+++ src/model/Student.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.awt.*;
+
+public class Student {
+
+    //Private variables for Student class
+    private String firstName;
+    private String lastName;
+    private int studentNum;
+    private String listOfFavActivities;
+    private Image image;
+
+    /**
+     * @param firstName - example Branden
+     * @param lastName - example Yoshaev
+     * @param studentNum - example 1137913
+     * @param listOfFavActivities - example Dancing, running, golfing
+     */
+
+}
