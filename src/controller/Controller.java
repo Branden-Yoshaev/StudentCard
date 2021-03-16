b78ae3bec70303c4ef952ef1a31f6bb927896327
Branden-Yoshaev <bsyoshae@lakeheadu.ca>
2021-03-15 22:19:01 -0400

    fxml components added

---
+++ src/controller/Controller.java
@@ -0,0 +1,40 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+            @FXML
+            private MenuBar studentCard;
+
+            @FXML
+            private Label firstName;
+
+            @FXML
+            private Label firstName_Label;
+
+            @FXML
+            private Label lastName;
+
+            @FXML
+            private Label lastName_Label;
+
+            @FXML
+            private Label studentNum;
+
+            @FXML
+            private Label studentNum_Label;
+
+            @FXML
+            private ImageView image;
+
+            @FXML
+            private Label favActivities;
+
+            @FXML
+            private ListView<?> listOfActivities;
+
+    }
